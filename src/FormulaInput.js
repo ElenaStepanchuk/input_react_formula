0ce4ee1e77e16b665476b11ea4605a797f595c8b
Elena <83465804+ElenaStepanchuk@users.noreply.github.com>
2024-06-25 14:58:03 +0300

    correct

--- src/FormulaInput.js
+++ src/FormulaInput.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import Autosuggest from "react-autosuggest";
 import * as math from "mathjs";
 import useStore from "./store";
 
 const fetchSuggestions = async (query) => {
   try {
     const parsed = math.parse(query);
+
+    console.log(parsed);
+
     const suggestions = parsed
       .filter((node) => node.isSymbolNode)
       .map((node) => node.name);
@@ -27,41 +29,39 @@ const FormulaInput = () => {
     enabled: !!inputValue,
   });
 
-  const getSuggestions = (value) => {
-    return suggestions || [];
-  };
-
-  const getSuggestionValue = (suggestion) => suggestion;
-
-  const renderSuggestion = (suggestion) => <div>{suggestion}</div>;
-
-  const onChange = (event, { newValue }) => {
-    setInputValue(newValue);
-    setFormula(newValue);
-  };
-
-  const onSuggestionsFetchRequested = ({ value }) => {
+  const handleChange = (e) => {
+    const value = e.target.value;
     setInputValue(value);
+    setFormula(value);
   };
 
-  const onSuggestionsClearRequested = () => {};
-
-  const inputProps = {
-    placeholder: "Enter formula...",
-    value: inputValue,
-    onChange: onChange,
+  const handleSelectSuggestion = (suggestion) => {
+    const newFormula = formula + suggestion;
+    setFormula(newFormula);
+    setInputValue("");
   };
 
   return (
     <div>
-      <Autosuggest
-        suggestions={getSuggestions(inputValue)}
-        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={onSuggestionsClearRequested}
-        getSuggestionValue={getSuggestionValue}
-        renderSuggestion={renderSuggestion}
-        inputProps={inputProps}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Enter formula..."
+        autocomplete="on"
       />
+      {suggestions && (
+        <ul>
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => handleSelectSuggestion(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
       <div>Formula: {formula}</div>
     </div>
   );
