ad78ad8d018feae781202bb0154210b76dda398d
Elena <83465804+ElenaStepanchuk@users.noreply.github.com>
2024-06-25 14:52:15 +0300

    add autos

--- src/FormulaInput.js
+++ src/FormulaInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import Autosuggest from "react-autosuggest";
 import * as math from "mathjs";
 import useStore from "./store";
 
@@ -26,38 +27,41 @@ const FormulaInput = () => {
     enabled: !!inputValue,
   });
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setInputValue(value);
-    setFormula(value);
+  const getSuggestions = (value) => {
+    return suggestions || [];
   };
 
-  const handleSelectSuggestion = (suggestion) => {
-    const newFormula = formula + suggestion;
-    setFormula(newFormula);
-    setInputValue("");
+  const getSuggestionValue = (suggestion) => suggestion;
+
+  const renderSuggestion = (suggestion) => <div>{suggestion}</div>;
+
+  const onChange = (event, { newValue }) => {
+    setInputValue(newValue);
+    setFormula(newValue);
+  };
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    setInputValue(value);
+  };
+
+  const onSuggestionsClearRequested = () => {};
+
+  const inputProps = {
+    placeholder: "Enter formula...",
+    value: inputValue,
+    onChange: onChange,
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Enter formula..."
+      <Autosuggest
+        suggestions={getSuggestions(inputValue)}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
       />
-      {suggestions && (
-        <ul>
-          {suggestions.map((suggestion) => (
-            <li
-              key={suggestion}
-              onClick={() => handleSelectSuggestion(suggestion)}
-            >
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
       <div>Formula: {formula}</div>
     </div>
   );
